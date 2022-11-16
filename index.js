ec8b55d26563f651015a5bf503071c691e7fbf47
Stephen J Learmonth <stephen.j.learmonth@gmail.com>
2022-11-16 19:37:34 +0000

    Add animation to button/key press

--- index.js
+++ index.js
@@ -7,12 +7,16 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 
     makeSound(buttonInnerHTML);
 
+    buttonAnimation(buttonInnerHTML);
+
   })
 }
 
 document.addEventListener("keypress", function (event) {
 
   makeSound(event.key);
+
+  buttonAnimation(event.key);
 
 });
 
@@ -50,4 +54,15 @@ function makeSound(key) {
     default:
       console.log(key);
   }
+}
+
+function buttonAnimation(currentKey) {
+
+  var activeButton = document.querySelector("." + currentKey);
+
+  activeButton.classList.add("pressed");
+
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
 }
